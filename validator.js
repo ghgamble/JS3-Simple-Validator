e324510f153ac943297625aed8f8e9a8de1435cd
Grace <grace.h.gamble@gmail.com>
2016-05-25 00:40:29 +0000

    JS3 Simple Validator

---
+++ validator.js
@@ -0,0 +1,34 @@
+//Part 1
+/*
+var phoneNumber = prompt("Enter your phone number, with dashes. This number won't be used for SPAM.");
+var regex = /^[0-9]{3}-[0-9]{3}-[0-9]{4}$/;
+
+alert(regex.test(phoneNumber));
+*/
+
+//Part 2
+/*
+var birthDate = prompt("What's your birthday?");
+var regex = /^[0-9]{1,2}\/[0-9]{1,2}\/[0-9]{2}$/;
+alert(regex.test(birthDate));
+*/
+
+//Part 3 THE ONE I DON'T UNDERSTAND!!!!!
+/*
+var postalCode = prompt("What's your postal code?");
+var regex = /^([0-9]{5}) | ([0-9]{5}[09]{2})$/;
+alert(regex.test(postalCode));
+*/
+
+//Part 4
+/*
+var stateAbbreviation = prompt("What's your state abbreviation?");
+if (stateAbbreviation === )
+*/
+
+//Part 5
+/*
+var married = prompt("Are you married?");
+var regex = /^(yes|no|YES|NO|Yes|No)$/;
+alert(regex.test(married));
+*/
